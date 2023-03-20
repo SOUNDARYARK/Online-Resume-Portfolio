8d921f13717f2625f3805f0bd7ed7f86cbd1ff07
SOUNDARYARK <93415995+SOUNDARYARK@users.noreply.github.com>
2023-03-20 10:04:18 +0530

    Add files via upload

---
+++ script/index.js
@@ -0,0 +1,11 @@
+let typed = new Typed(".typing", {
+    strings: [ "",
+      " SOUNDARYA R",
+      " Web Devloper",
+      " Web Designer",
+      " Frontend Developer.",
+    ],
+    typeSpeed: 150,
+    backSpeed: 80,
+    loop: true,
+  });
